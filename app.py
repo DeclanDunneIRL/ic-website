d2468fb3a32f28ba0cc970b99e05026bf6f79ab1
Declan Dunne <dunnedeclan@gmail.com>
2023-07-15 22:29:00 +0100

    changed entry point

--- app.py
+++ app.py
@@ -3,11 +3,12 @@ import flask
 # Create a simple flask app that calles index.html
 app = flask.Flask(__name__)
 
+
 @app.route('/')
 def index():
     return flask.render_template('index.html')
 
+
 # Run the app
 if __name__ == '__main__':
     app.run(debug=True)
-
