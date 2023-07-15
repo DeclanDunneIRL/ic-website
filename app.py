610255ead5d602e6328fea451de181127f479f0f
Declan Dunne <dunnedeclan@gmail.com>
2023-07-15 18:34:58 +0100

    first commit

---
+++ app.py
@@ -0,0 +1,13 @@
+import flask
+
+# Create a simple flask app that calles index.html
+app = flask.Flask(__name__)
+
+@app.route('/')
+def index():
+    return flask.render_template('index.html')
+
+# Run the app
+if __name__ == '__main__':
+    app.run(debug=True)
+
